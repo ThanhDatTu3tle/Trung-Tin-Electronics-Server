2c68df87183f91cb9f5df10cf214c798c40c09ac
ThanhDatTu3tle <thanhdat11badinh@gmail.com>
2024-02-27 11:16:48 +0700

    update url

--- src/main/java/com/example/BE_LinkKien/configuration/WebConfig.java
+++ src/main/java/com/example/BE_LinkKien/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.0.110:3000", "http://192.168.1.89:3000", "http://localhost:3000", "https://trung-tin-electronics-client.vercel.app")
+                .allowedOrigins("http://192.168.0.111:3000", "http://192.168.1.89:3000", "http://localhost:3000", "https://trung-tin-electronics-client.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization") // Nếu bạn muốn bổ sung thêm header cho response
